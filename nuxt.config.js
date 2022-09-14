64f5ba0a5f178993efacc6cf87d9ecca1fb8b7a9
George Ndirangu <ndirangu.mepawa@gmail.com>
2022-09-14 18:03:18 +0300

    add deploy script

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: 'lighthouse-tech-challenge'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'lighthouse-tech-challenge',
