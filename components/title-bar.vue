03272261fb31503ec63a78fc27d11099f37a486f
George Ndirangu <ndirangu.mepawa@gmail.com>
2022-09-14 17:50:43 +0300

    feat: landing page

--- components/title-bar.vue
+++ components/title-bar.vue
@@ -1,16 +1,16 @@
 <template>
   <c-flex p="2" class="title-bar" justify="space-between">
     <c-button variant="unstyled" aria-label="notifications">
-      <img  src="~/assets/icons/ho_me.png" />
+      <img src="~/assets/icons/ho_me.png" />
     </c-button>
 
     <img size="150px" src="~/assets/icons/ho_lo.png" />
 
     <c-menu>
       <c-menu-button variant="unstyled">
-        <img  src="~/assets/icons/ho_li.png" />
+        <img src="~/assets/icons/ho_li.png" />
       </c-menu-button>
-      <c-menu-list minW="100px">
+      <c-menu-list min-w="100px">
         <c-menu-item>Item1</c-menu-item>
         <c-menu-item>Item2</c-menu-item>
         <c-menu-item>Item3</c-menu-item>
